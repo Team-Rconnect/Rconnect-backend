66d58e4f4fd003074979c822c0b699f162b2abae
jogarao123 <92026613+jogarao123@users.noreply.github.com>
2022-04-02 19:03:07 +0530

    added user schema and sample get api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const UserModel = require("./models/User");
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +13,16 @@ mongoose.connect(
   "mongodb+srv://jogarao:1234@cluster1.tlddu.mongodb.net/Rconnect?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
+//sample insertion - tobe removed
+app.get("/", async (req, res) => {
+  const user = {
+    firstName: "charan",
+    lastName: "vinay",
+  };
+  const userToInsert = new UserModel(user);
+  await userToInsert.save();
+  res.send(userToInsert);
+});
 
 app.listen(3001, () => {
   console.log("you are connected");
--- models/User.js
+++ models/User.js
@@ -1,1 +1,41 @@
 const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  connections: {
+    type: Number,
+    default: 0,
+  },
+  dob: Date,
+  title: String,
+  about: String,
+  skills: [String],
+  experience: [
+    {
+      from: Date,
+      to: Date,
+      organization: String,
+      description: String,
+    },
+  ],
+  experience: [
+    {
+      from: Date,
+      to: Date,
+      university: String,
+      description: String,
+    },
+  ],
+  projects: [String],
+});
+
+const UserModel = mongoose.model("users", User);
+
+module.exports = UserModel;
